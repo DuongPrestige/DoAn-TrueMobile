c094394bd89af16deae751583b248a5b4218fbe9
Duong Prestige <156173100+DuongPrestige@users.noreply.github.com>
2024-04-14 11:43:23 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+import cors from 'cors'
+import express from 'express'
+require('dotenv').config()
+
+import initRoute from './src/routes'
+
+require('./src/config/connection_db')
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    method: ['GET, POST, OPTIONS, PUT, PATCH, DELETE']
+}))
+
+//doc du lieu kieu json
+app.use(express.json())
+//doc du lieu mang, object 
+app.use(express.urlencoded({extended: true}))
+
+initRoute(app)
+
+
+const PORT = process.env.PORT || 2007
+const listener = app.listen(PORT, () => {
+    console.log('Server is running on the port ' + listener.address().port);
+})
