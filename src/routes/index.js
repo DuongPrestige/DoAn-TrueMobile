fdb05eccec8cc4346f16971a5e867313fc1e0007
DuongLap <duong.prestige86@gmail.com>
2024-05-14 16:05:12 +0700

    add statistic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,72 +1,73 @@
-import userRoute from './userRoute'
-import allCodeRoute from './allCodeRoute'
-import productRoute from './productRoute'
-import bannerRoute from './bannerRoute'
-import blogRoute from './blogRoute'
-import typeshipRoute from './typeshipRoute'
-import typevoucherRoute from './typevoucherRoute'
-import voucherRoute from './voucherRoute'
-import reviewRoute from './reviewRoute'
-import shopcartRoute from './shopcartRoute'
-import receiptRoute from './receiptRoute'
-import addressUserRoute from './addressUserRoute'
-import messageRoute from './messageRoute'
+import userRoute from "./userRoute";
+import allCodeRoute from "./allCodeRoute";
+import productRoute from "./productRoute";
+import bannerRoute from "./bannerRoute";
+import blogRoute from "./blogRoute";
+import typeshipRoute from "./typeshipRoute";
+import typevoucherRoute from "./typevoucherRoute";
+import voucherRoute from "./voucherRoute";
+import reviewRoute from "./reviewRoute";
+import shopcartRoute from "./shopcartRoute";
+import receiptRoute from "./receiptRoute";
+import addressUserRoute from "./addressUserRoute";
+import messageRoute from "./messageRoute";
 
-import supplierRoute from './supplierRoute'
-import orderRoute from './orderRoute'
+import supplierRoute from "./supplierRoute";
+import orderRoute from "./orderRoute";
+import statisticRoute from "./statisticRoute";
 
-import {internalServer,notFound} from '../middlewares/handle_error'
+import { internalServer, notFound } from "../middlewares/handle_error";
 
 const initRoute = (app) => {
+  //=====================API USER==========================//
+  app.use("/api/v1/user", userRoute);
 
-    //=====================API USER==========================//
-    app.use('/api/v1/user', userRoute)
+  //===================API ALLCODE========================//
+  app.use("/api/v1/all-code", allCodeRoute);
 
-    //===================API ALLCODE========================//
-    app.use('/api/v1/all-code', allCodeRoute)
+  //===================API PRODUCT========================//
+  app.use("/api/v1/product", productRoute);
 
-    //===================API PRODUCT========================//
-    app.use('/api/v1/product', productRoute)
+  //===================API BANNER========================//
+  app.use("/api/v1/banner", bannerRoute);
 
-    //===================API BANNER========================//
-    app.use('/api/v1/banner', bannerRoute)
+  //===================API BLOG========================//
+  app.use("/api/v1/blog", blogRoute);
 
-    //===================API BLOG========================//
-    app.use('/api/v1/blog', blogRoute)
+  //===================API TYPESHIP========================//
+  app.use("/api/v1/typeship", typeshipRoute);
 
-    //===================API TYPESHIP========================//
-    app.use('/api/v1/typeship', typeshipRoute)
+  //===================API TYPEVOUCHER========================//
+  app.use("/api/v1/typevoucher", typevoucherRoute);
 
-    //===================API TYPEVOUCHER========================//
-    app.use('/api/v1/typevoucher', typevoucherRoute)
+  //===================API VOUCHER========================//
+  app.use("/api/v1/voucher", voucherRoute);
 
-    //===================API VOUCHER========================//
-    app.use('/api/v1/voucher', voucherRoute)
-    
-    //===================API REVIEW========================//
-    app.use('/api/v1/review', reviewRoute)
+  //===================API REVIEW========================//
+  app.use("/api/v1/review", reviewRoute);
 
-    //===================API SHOPCART========================//
-    app.use('/api/v1/shopcart', shopcartRoute)
+  //===================API SHOPCART========================//
+  app.use("/api/v1/shopcart", shopcartRoute);
 
+  //===================API SUPPLIER========================//
+  app.use("/api/v1/supplier", supplierRoute);
 
-    //===================API SUPPLIER========================//
-    app.use('/api/v1/supplier', supplierRoute)
+  //===================API RECEIPT========================//
+  app.use("/api/v1/receipt", receiptRoute);
 
-    //===================API RECEIPT========================//
-    app.use('/api/v1/receipt', receiptRoute)
+  //===================API ADDRESS USER========================//
+  app.use("/api/v1/addressuser", addressUserRoute);
 
-    //===================API ADDRESS USER========================//
-    app.use('/api/v1/addressuser', addressUserRoute)
+  //===================API MESSAGE========================//
+  app.use("/api/v1/message", messageRoute);
 
-    //===================API MESSAGE========================//
-    app.use('/api/v1/message', messageRoute)
+  //===================API MESSAGE========================//
+  app.use("/api/v1/order", orderRoute);
 
-    //===================API MESSAGE========================//
-    app.use('/api/v1/order', orderRoute)
-    
-    app.use(notFound)
+  //===================API statistic========================//
+  app.use("/api", statisticRoute);
 
-}
+  app.use(notFound);
+};
 
-module.exports = initRoute
+module.exports = initRoute;
